2a87ac4b539d7adc65b855dfabb2697d6a65472e
ccarsonclay <carsonaclay@gmail.com>
2023-07-19 18:30:51 -0400

    adding app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,52 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import './App.css';
+
+function App() 
+  const [movies, setMovies] = useState([]);
+
+  const addReview = async (title, rating, review) => {
+    try {
+      const response = await axios.get(`http://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=3e71c3e0`);
+      const movieData = response.data;
+  
+      if (movieData.Response === 'True') {
+        const imdbID = movieData.imdbID;
+        const posterResponse = await axios.get(`http://www.omdbapi.com/?i=${encodeURIComponent(imdbID)}&apikey=3e71c3e0`);
+        const posterData = posterResponse.data;
+  
+        const newMovie = {
+          title,
+          rating,
+          review,
+          poster: posterData.Poster,
+        };
+  
+        setMovies(prevMovies => [...prevMovies, newMovie]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="app">
+      <header>
+        <h1>Carbonboxd</h1>
+      </header>
+      <main>
+        {/* Display movie list */}
+        <section id="movies">
+          <h2>Top Movies</h2>
+          <div className="movie-list">
+            {movies.map((movie, index) => (
+              <div key={index} className="movie-card">
+              <h3>{movie.title}</h3>
+              <img src={movie.poster} alt={movie.title} />
+              <p>{movie.review}</p>
+              <p>Rating: {movie.rating}</p>
+            </div>
+            ))}
+          </div>
+          </main>
+        </section> 
