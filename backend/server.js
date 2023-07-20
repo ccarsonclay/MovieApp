c927a3e1fb6e98ba98ef979f2e6da69a9950e41b
ccarsonclay <carsonaclay@gmail.com>
2023-07-20 00:48:37 -0400

    js overhaul

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+const port = 3000;
+const url = 'mongodb://localhost:27017'; 
+const databaseName = 'mydatabase'; 
+
+app.use(express.json());
+
+MongoClient.connect(url, (err, client) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+
+  const db = client.db(databaseName);
+  console.log('Connected to MongoDB');
+
+  // api endpoints
+  app.post('/reviews', (req, res) => {
+    const { movieId, reviewText, rating } = req.body;
+    const review = { movieId, reviewText, rating };
+
+    db.collection('reviews')
+      .insertOne(review)
+      .then(() => {
+        console.log('Review inserted successfully');
+        res.status(201).json({ message: 'Review stored successfully' });
+      })
+      .catch((err) => {
+        console.error('Error inserting review:', err);
+        res.status(500).json({ error: 'An error occurred while saving the review' });
+      });
+  });
+
+  // server start
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,82 +1,131 @@
-import axios from 'axios';
 import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('');
+  const [reviews, setReviews] = useState([]);
+  const [reviewContent, setReviewContent] = useState('');
+  const [score, setScore] = useState('');
 
-  const addReview = async (title, rating, review) => {
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
     try {
-      const response = await axios.get(`http://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=3e71c3e0`);
-      const movieData = response.data;
-  
-      if (movieData.Response === 'True') {
-        const imdbID = movieData.imdbID;
-        const posterResponse = await axios.get(`http://www.omdbapi.com/?i=${encodeURIComponent(imdbID)}&apikey=3e71c3e0`);
-        const posterData = posterResponse.data;
-  
-        const newMovie = {
-          title,
-          rating,
-          review,
-          poster: posterData.Poster,
-        };
-  
-        setMovies(prevMovies => [...prevMovies, newMovie]);
+      const response = await axios.get(
+        `http://www.omdbapi.com/?s=${encodeURIComponent(
+          searchTerm
+        )}&apikey=3e71c3e0`
+      );
+      const data = response.data;
+
+      if (data.Response === 'True') {
+        const moviesWithPosters = await Promise.all(
+          data.Search.map(async (movie) => {
+            const posterResponse = await axios.get(
+              `http://www.omdbapi.com/?i=${encodeURIComponent(
+                movie.imdbID
+              )}&apikey=3e71c3e0`
+            );
+            const posterData = posterResponse.data;
+
+            return {
+              id: movie.imdbID,
+              title: movie.Title,
+              poster: posterData.Poster,
+            };
+          })
+        );
+
+        setMovies(moviesWithPosters);
+        setError('');
+      } else {
+        setMovies([]);
+        setError(data.Error);
       }
     } catch (error) {
       console.error(error);
+      setMovies([]);
+      setError('An error occurred while fetching the movies');
     }
   };
-  
+
+  const handleReviewSubmit = (e, movieId, reviewContent) => {
+    e.preventDefault();
+
+    const newReview = {
+      movieId: movieId,
+      content: reviewContent,
+      score: score,
+    };
+
+    setReviews([...reviews, newReview]);
+    setReviewContent('');
+    setScore('');
+  };
+
   return (
-    <div className="app">
-      <header>
-        <h1>MovieBoxxd</h1>
-      </header>
-      <main>
-        {/* Display movie list */}
-        <section id="movies">
-          <h2>Top Movies</h2>
-          <div className="movie-list">
-            {movies.map((movie, index) => (
-              <div key={index} className="movie-card">
-              <h3>{movie.title}</h3>
+    <div className="container">
+      <h1>Movie Review App</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search movies..."
+          required
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {error ? (
+        <p className="error">{error}</p>
+      ) : (
+        <div className="movies">
+          {movies.map((movie) => (
+            <div key={movie.id} className="movie-card">
               <img src={movie.poster} alt={movie.title} />
-              <p>{movie.review}</p>
-              <p>Rating: {movie.rating}</p>
+              <h2>{movie.title}</h2>
+
+              <form onSubmit={(e) => handleReviewSubmit(e, movie.id, reviewContent)}>
+                <input
+                  type="text"
+                  value={reviewContent}
+                  onChange={(e) => setReviewContent(e.target.value)}
+                  placeholder="Write a review..."
+                  required
+                />
+                <input
+                  type="number"
+                  min="1"
+                  max="10"
+                  placeholder="Score (1-10)"
+                  value={score}
+                  onChange={(e) => setScore(e.target.value)}
+                  required
+                />
+                <button type="submit">Submit Review</button>
+              </form>
+
+              <div className="reviews">
+                {reviews.map((review, index) => {
+                  if (review.movieId === movie.id) {
+                    return (
+                      <div key={index}>
+                        <p>{review.content}</p>
+                        <p>Score: {review.score}</p>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
             </div>
-            ))}
-          </div>
-        </section>
-
-        {/* Review form */}
-        <section id="add-review-form">
-          <h2>Add a Review</h2>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              const { title, rating, review } = e.target;
-              addReview(title.value, Number(rating.value), review.value);
-              e.target.reset();
-            }}
-          >
-            <label htmlFor="movie-title">Movie Title:</label>
-            <input type="text" id="movie-title" required />
-
-            <label htmlFor="rating">Rating:</label>
-            <input type="number" id="rating" min="0" max="5" step="0.1" required />
-
-            <label htmlFor="review">Review:</label>
-            <textarea id="review" />
-            
-            <button type="submit">Submit Review</button>
-          </form>
-        </section>
-      </main>
-      <footer>
-        &copy; 2023 Movie Reviews
-      </footer>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
